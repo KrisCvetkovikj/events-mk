718ddd69046910f258ed534ffbc9903813fd38d8
Kristijan Cvetkovikj <kristijan.cvetkovikj@one-inside.com>
2016-06-10 20:10:00 +0200

    merged fe and be

--- app.js
+++ app.js
@@ -1,22 +1,100 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-var debug = require('debug')('fb-event-location-search:server');
-var http = require('http');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var routes = require('./routes/events');
+// ROUTING VARIABLES
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var events = require('./routes/events');
+var db = require("./routes/db");
 
 var app = express();
 
-app.use(logger('dev'));
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/node_modules', express.static(__dirname + "/node_modules"));
+app.use('/css', express.static(__dirname + "/css"));
+app.use('/js', express.static(__dirname + "/js"));
+app.use('/fonts', express.static(__dirname + "/fonts"));
+app.use('/img', express.static(__dirname + "/img"));
 
-app.get('/', function(req, res) {
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/events', events);
+
+app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.use('/', routes);
+app.get('/test', function (req, res) {
+	res.render('test');
+});
 
-var port = process.env.PORT || 3000;
+app.get('/db/getUsers', function (req, res) {
+	db.getUsers(req, res);
+});
+app.get('/db/addUser', function (req, res) {
+	db.addUser(req, res);
+});
+app.get('/db/getUserFavorites', function (req, res) {
+	db.getUserFavorites(req, res);
+});
+app.get('/db/addFavorite', function (req, res) {
+	db.addFavorite(req, res);
+});
+app.get('/db/removeFavorite', function (req, res) {
+	db.removeFavorite(req, res);
+});
+app.get('/db/createTable', function (req, res) {
+	db.createTable(req, res);
+});
+app.get('/db/dropTable', function (req, res) {
+	db.dropTable(req, res);
+});
 
-app.listen(port);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
+
+
+module.exports = app;
